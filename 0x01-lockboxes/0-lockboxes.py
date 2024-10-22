7a44b51f945676e9f3657e214988da20598a3fd0
Joseph <abangjosef@gmail.com>
2024-10-22 05:42:15 +0100

    Fix pycodestyle errors

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,13 +3,14 @@
     Solution to the LockBoxes problem
 """
 
+
 def canUnlockAll(boxes):
     """
     Checks whether all boxes can be unlocked.
-    
+
     Args:
     boxes: A list of lists, where each sublist contains keys to other boxes.
-    
+
     Returns:
     True if every box can be opened, False otherwise.
     """
@@ -22,12 +23,12 @@ def canUnlockAll(boxes):
         box_to_check = current_box
         unlocked_boxes.append(current_box)
         keys.update(boxes[current_box])
-        
+
         for key in keys:
             if key != 0 and key < num_boxes and key not in unlocked_boxes:
                 current_box = key
                 break
-        
+
         if box_to_check != current_box:
             continue
         else:
@@ -37,5 +38,5 @@ def canUnlockAll(boxes):
     for i in range(num_boxes):
         if i not in unlocked_boxes and i != 0:
             return False
-    
+
     return True
